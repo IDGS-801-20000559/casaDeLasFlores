55cdcd9415f65caf91a2f1c4925e9771a6bfa711
Jonathan IDGS-801-20000559 <20000559@alumnos.utleon.edu.mx>
2023-03-31 16:36:58 -0600

    Primer Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from db import get_connection
+from flask import Flask, redirect, jsonify, render_template
+from flask import request
+from flask import url_for
+from config import DevelopmentConfig
+
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def index():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=3000)
